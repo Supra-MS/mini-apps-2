8afda599d3a7db41cb4d00fa2d2fa1f21a95d359
Supra-MS <suprabharathi.m.s@gmail.com>
2020-10-29 07:25:58 -0700

    Fix date logic

--- challenge_1/client/utils/CalcDate.js
+++ challenge_1/client/utils/CalcDate.js
@@ -9,17 +9,24 @@ export function CalcDate(dated) {
 
     let yr = splitBySlash[0];
     let formatTemp = copy.join('/');
-    date.setYear(yr);
-
-    let yearsAgo = moment(date, 'YYYYMMDD').fromNow();
-    let newDate = moment(formatTemp, 'MMDD').format("MMMM YYYY");
     let isBC = Number(yr) < 0;
     
     if (isBC) {
-        return (len === 1) ? `B.C ${-Number(yr)} [${yearsAgo}]` 
-                : `B.C ${-Number(yr)} ${newDate} [${yearsAgo}]`;
+        date.setYear(yr);
+        let yearsAgo = moment(date, 'YYYYMMDD').fromNow();
+        let newDate = moment(formatTemp, 'MMDD').format("MMMM");
+        if (len === 1) {
+            return `B.C ${-Number(yr)} [${yearsAgo}]` 
+        } else {
+            return `B.C ${-Number(yr)} ${newDate} [${yearsAgo}]`;
+        }
     } else {
-        return (len === 1) ? `A.D ${yr} [${yearsAgo}]` 
-                : `A.D ${yr} ${newDate} [${yearsAgo}]`;
+        let newDate = moment(formatTemp, 'MMDD').format("MMMM");
+        let yearsAgo =  2020 - Number(yr);
+        if (len === 1) {
+            return `A.D ${yr} [${yearsAgo} years ago]` 
+        } else {
+            return `A.D ${yr} ${newDate} [${yearsAgo} years ago]`;
+        }
     }
 };
