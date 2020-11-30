383a01a2ca60e20b62a929b813ad0f51a34eafa8
Supra-MS <suprabharathi.m.s@gmail.com>
2020-11-30 12:24:14 -0800

    Initial Setup

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,11 @@
+const express = require('express');
+const path = require('path');
+const PORT = 3002;
+const app = express();
 
+app.use(express.json());
+app.use(express.static(path.join(__dirname, './public')));
 
+app.listen(PORT, () => {
+  console.log(`*** Server listening on port http://localhost:${PORT} ***`);
+});
